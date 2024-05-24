bc5ddcda9794da67a54156bb4715aa24d1b192ef
BRUNOANDREJUSTEN <BRUNOANDREJUSTEN@SCS216410>
2024-05-23 21:10:15 -0300

    aula 1

---
+++ Comandas/FrmPrincipal.Designer.cs
@@ -0,0 +1,185 @@
+﻿namespace Comandas
+{
+    partial class FrmPrincipal
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            dreamForm1 = new ReaLTaiizor.Forms.DreamForm();
+            button3 = new ReaLTaiizor.Controls.Button();
+            button2 = new ReaLTaiizor.Controls.Button();
+            button1 = new ReaLTaiizor.Controls.Button();
+            button4 = new ReaLTaiizor.Controls.Button();
+            foreverGroupBox1 = new ReaLTaiizor.Controls.ForeverGroupBox();
+            dreamForm1.SuspendLayout();
+            foreverGroupBox1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // dreamForm1
+            // 
+            dreamForm1.ColorA = Color.FromArgb(40, 218, 255);
+            dreamForm1.ColorB = Color.FromArgb(63, 63, 63);
+            dreamForm1.ColorC = Color.FromArgb(41, 41, 41);
+            dreamForm1.ColorD = Color.FromArgb(27, 27, 27);
+            dreamForm1.ColorE = Color.FromArgb(0, 0, 0, 0);
+            dreamForm1.ColorF = Color.FromArgb(25, 255, 255, 255);
+            dreamForm1.Controls.Add(foreverGroupBox1);
+            dreamForm1.Dock = DockStyle.Fill;
+            dreamForm1.Location = new Point(0, 0);
+            dreamForm1.Name = "dreamForm1";
+            dreamForm1.Size = new Size(790, 448);
+            dreamForm1.TabIndex = 0;
+            dreamForm1.TabStop = false;
+            dreamForm1.TitleAlign = HorizontalAlignment.Center;
+            dreamForm1.TitleHeight = 25;
+            dreamForm1.Enter += dreamForm1_Enter;
+            // 
+            // button3
+            // 
+            button3.BackColor = Color.Transparent;
+            button3.BorderColor = Color.FromArgb(32, 34, 37);
+            button3.EnteredBorderColor = Color.FromArgb(165, 37, 37);
+            button3.EnteredColor = Color.FromArgb(32, 34, 37);
+            button3.Font = new Font("Microsoft Sans Serif", 12F);
+            button3.Image = null;
+            button3.ImageAlign = ContentAlignment.MiddleLeft;
+            button3.InactiveColor = Color.FromArgb(32, 34, 37);
+            button3.Location = new Point(372, 53);
+            button3.Name = "button3";
+            button3.PressedBorderColor = Color.FromArgb(165, 37, 37);
+            button3.PressedColor = Color.FromArgb(165, 37, 37);
+            button3.Size = new Size(151, 75);
+            button3.TabIndex = 2;
+            button3.Text = "v";
+            button3.TextAlignment = StringAlignment.Center;
+            // 
+            // button2
+            // 
+            button2.BackColor = Color.Transparent;
+            button2.BorderColor = Color.FromArgb(32, 34, 37);
+            button2.EnteredBorderColor = Color.FromArgb(165, 37, 37);
+            button2.EnteredColor = Color.FromArgb(32, 34, 37);
+            button2.Font = new Font("Microsoft Sans Serif", 12F);
+            button2.Image = null;
+            button2.ImageAlign = ContentAlignment.MiddleLeft;
+            button2.InactiveColor = Color.FromArgb(32, 34, 37);
+            button2.Location = new Point(200, 53);
+            button2.Name = "button2";
+            button2.PressedBorderColor = Color.FromArgb(165, 37, 37);
+            button2.PressedColor = Color.FromArgb(165, 37, 37);
+            button2.Size = new Size(151, 75);
+            button2.TabIndex = 1;
+            button2.Text = "v";
+            button2.TextAlignment = StringAlignment.Center;
+            // 
+            // button1
+            // 
+            button1.BackColor = Color.Transparent;
+            button1.BorderColor = Color.FromArgb(32, 34, 37);
+            button1.EnteredBorderColor = Color.FromArgb(165, 37, 37);
+            button1.EnteredColor = Color.FromArgb(32, 34, 37);
+            button1.Font = new Font("Microsoft Sans Serif", 12F);
+            button1.Image = null;
+            button1.ImageAlign = ContentAlignment.MiddleLeft;
+            button1.InactiveColor = Color.FromArgb(32, 34, 37);
+            button1.Location = new Point(29, 53);
+            button1.Name = "button1";
+            button1.PressedBorderColor = Color.FromArgb(165, 37, 37);
+            button1.PressedColor = Color.FromArgb(165, 37, 37);
+            button1.Size = new Size(151, 75);
+            button1.TabIndex = 0;
+            button1.Text = "v";
+            button1.TextAlignment = StringAlignment.Center;
+            // 
+            // button4
+            // 
+            button4.BackColor = Color.Transparent;
+            button4.BorderColor = Color.FromArgb(32, 34, 37);
+            button4.EnteredBorderColor = Color.FromArgb(165, 37, 37);
+            button4.EnteredColor = Color.FromArgb(32, 34, 37);
+            button4.Font = new Font("Microsoft Sans Serif", 12F);
+            button4.Image = null;
+            button4.ImageAlign = ContentAlignment.MiddleLeft;
+            button4.InactiveColor = Color.Black;
+            button4.Location = new Point(547, 53);
+            button4.Name = "button4";
+            button4.PressedBorderColor = Color.FromArgb(165, 37, 37);
+            button4.PressedColor = Color.FromArgb(165, 37, 37);
+            button4.Size = new Size(151, 75);
+            button4.TabIndex = 3;
+            button4.Text = "v";
+            button4.TextAlignment = StringAlignment.Center;
+            // 
+            // foreverGroupBox1
+            // 
+            foreverGroupBox1.ArrowColorF = Color.FromArgb(60, 70, 73);
+            foreverGroupBox1.ArrowColorH = Color.FromArgb(60, 70, 73);
+            foreverGroupBox1.BackColor = Color.Transparent;
+            foreverGroupBox1.BaseColor = Color.FromArgb(60, 70, 73);
+            foreverGroupBox1.Controls.Add(button3);
+            foreverGroupBox1.Controls.Add(button4);
+            foreverGroupBox1.Controls.Add(button1);
+            foreverGroupBox1.Controls.Add(button2);
+            foreverGroupBox1.Font = new Font("Segoe UI", 10F);
+            foreverGroupBox1.Location = new Point(21, 186);
+            foreverGroupBox1.Name = "foreverGroupBox1";
+            foreverGroupBox1.ShowArrow = true;
+            foreverGroupBox1.ShowText = true;
+            foreverGroupBox1.Size = new Size(738, 225);
+            foreverGroupBox1.TabIndex = 4;
+            foreverGroupBox1.TextColor = Color.FromArgb(35, 168, 109);
+            foreverGroupBox1.Click += this.foreverGroupBox1_Click;
+            // 
+            // FrmPrincipal
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(790, 448);
+            Controls.Add(dreamForm1);
+            FormBorderStyle = FormBorderStyle.None;
+            MaximumSize = new Size(1920, 1020);
+            MinimumSize = new Size(261, 65);
+            Name = "FrmPrincipal";
+            ShowIcon = false;
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "dungeonForm1";
+            TransparencyKey = Color.Fuchsia;
+            Load += principal_Load;
+            dreamForm1.ResumeLayout(false);
+            foreverGroupBox1.ResumeLayout(false);
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private ReaLTaiizor.Forms.DreamForm dreamForm1;
+        private ReaLTaiizor.Controls.Button button3;
+        private ReaLTaiizor.Controls.Button button2;
+        private ReaLTaiizor.Controls.Button button1;
+        private ReaLTaiizor.Controls.Button button4;
+        private ReaLTaiizor.Controls.ForeverGroupBox foreverGroupBox1;
+    }
+}
---
+++ Comandas/FrmPrincipal.cs
@@ -0,0 +1,24 @@
+namespace Comandas
+{
+    public partial class FrmPrincipal : Form
+    {
+        public FrmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void foreverForm1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void principal_Load(object sender, EventArgs e)
+        {
+        }
+
+        private void dreamForm1_Enter(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
